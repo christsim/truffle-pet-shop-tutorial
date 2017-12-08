82fbb64b66c27ccac99cec1bb9607d7a5c082c9a
Chris <christsim@gmail.com>
2017-12-08 10:28:43 +0200

    added truffle compile to package.json Added initial adoption solidity file

---
+++ contracts/Adoption.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.4; // The minimum version of Solidity required
+
+contract Adoption {
+
+  address[16] public adopters;  // fixed array of 16 ethereum addresses
+  // public member variables have automatic getter methods
+  // array public variables require and id, so like
+  // function getAdopter(index) { return adopters[index];}
+
+  // adopt a pet
+  function adopt(uint petId) public returns (uint) {
+      require(petId >= 0 && petId <= 15); //we only have 16 pets
+
+      // msg.sender is the senders address (person or smart contract)
+      adopters[petId] = msg.sender;    
+
+      return petId;
+  }
+
+  //return all the adopters, unlike the auto-magic getter
+  function getAdopters() public returns (address[16]) {
+    return adopters;
+  }
+
+  
+
+}
